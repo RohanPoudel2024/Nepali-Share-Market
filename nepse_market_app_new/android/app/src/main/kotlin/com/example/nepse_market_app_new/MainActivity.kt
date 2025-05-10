e9b12c6843fb8954b87fcf6593f0476d54819b82
Rohan Poudel <np05cp4s240029@iic.edu.np>
2025-05-10 11:42:29 +0545

    nc

--- nepse_market_app_new/android/app/src/main/kotlin/com/example/nepse_market_app_new/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nepse_market_app_new
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- nepse_market_app_new/lib/api/cookie_manager_web.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dio/dio.dart';
-
-// Empty implementation for web
-Interceptor? createCookieManager() {
-  // Return null for web platforms
-  return null;
-}
